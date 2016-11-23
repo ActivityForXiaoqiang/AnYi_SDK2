9be03252d1e8a21a0f6e170a9fb05638505c13fa
ActivityForXiaoqiang <xiaoqiang_lol@163.com>
2016-11-23 16:30:34 +0800

    初始化

---
+++ paysdk/src/main/java/com/union/paysdk/Tools.java
@@ -0,0 +1,16 @@
+package com.union.paysdk;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by xiaoqiang on 16/11/23.
+ */
+
+public class Tools {
+
+
+    public static void show(Context context) {
+        Toast.makeText(context, "test....", Toast.LENGTH_SHORT).show();
+    }
+}
